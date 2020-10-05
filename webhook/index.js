95805391ef3e2b1772c540977010062e79539d1b
aemurill <murilloaxel42@gmail.com>
2020-10-05 12:36:06 -0700

    webhookfix

--- webhook/index.js
+++ webhook/index.js
@@ -4,9 +4,6 @@ const webhook = (req, res) => ***REMOVED***
   let body = req.body
 
   // Retrieving parameters from the request made by the agent
-  let action = body.result.action
-  let parameters = body.result.parameters
-
   // Performing the action
   /*if (action === 'fetchPriceCryptoCurrency') ***REMOVED***
     // Fetch the price of the cryptocurrency

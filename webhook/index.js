7a870c6c124bd52a2e3c4113cafaa6ef69f63fbd
aemurill <murilloaxel42@gmail.com>
2020-10-07 11:40:42 -0700

    log-in

--- webhook/index.js
+++ webhook/index.js
@@ -19,6 +19,35 @@ const test = (req, res) => ***REMOVED***
         "messages": [***REMOVED***
           "text": ***REMOVED***
             "text": [
+              "test"
+            ]
+          }
+        }],
+        "merge_behavior": "REPLACE"
+      }
+    }
+  );
+};
+
+const login = (req, res) => ***REMOVED***
+  let body = req.body;
+  console.log(body);
+
+  // Retrieving parameters from the request made by the agent
+  // Performing the action
+  /*if (action === 'fetchPriceCryptoCurrency') ***REMOVED***
+    // Fetch the price of the cryptocurrency
+    let price = ...
+    let response = ...
+  }*/
+
+  // Sending back the results to the agent
+  res.json(
+    ***REMOVED***
+      "fulfillment_response": ***REMOVED***
+        "messages": [***REMOVED***
+          "text": ***REMOVED***
+            "text": [
               "Login Successful"
             ]
           }
@@ -26,16 +55,14 @@ const test = (req, res) => ***REMOVED***
         "merge_behavior": "REPLACE"
       },
       "session_info": ***REMOVED***
-        "parameters": ***REMOVED***
-
-        }
-
+        "session" : body.sessionInfo.session,
+        "parameters": ***REMOVED***"loggedIn" : true}
       }
     }
   );
-}
+};
 
 module.exports = ***REMOVED***
-  test: test
-
-}
+  test: test,
+  login: login
+};

8cfa30f7ac7f1c439aecd3cce3a1c3f61615c4aa
aemurill <murilloaxel42@gmail.com>
2020-10-07 15:28:44 -0700

    validation1

--- webhook/index.js
+++ webhook/index.js
@@ -34,44 +34,77 @@ const test = (req, res) => ***REMOVED***
 };
 
 const login = (req, res) => ***REMOVED***
-  let body = req.body;
-  console.log(body);
+  let req_body = req.body;
+  console.log(req_body);
 
-  // Retrieving parameters from the request made by the agent
-  // Performing the action
-  /*if (action === 'fetchPriceCryptoCurrency') ***REMOVED***
-    // Fetch the price of the cryptocurrency
-    let price = ...
-    let response = ...
-  }*/
+  let req_session_info = null;
+  let req_parameters = null;
+  let req_email = null;
+  let req_password = null;
+  let valid = false;
+  if (body.session_info != null)***REMOVED***
+    req_session_info = body.session_info;
+    if (req_session_info.parameters != null)***REMOVED***
+      req_parameters = req_session_info.parameters;
+      if (req_parameters.email != null && req_parameters.password != null)***REMOVED***
+        req_email = req_parameters.email;
+        req_password = req_parameters.email;
+        if(req_email== "test@gmail.com" && req_password == "abc123") 
+          valid = true; 
+      }
+    }
+  }
 
-  // Sending back the results to the agent
-  var json_msg = ***REMOVED***
-    "fulfillment_response": ***REMOVED***
-      "messages": [***REMOVED***
-        "text": ***REMOVED***
-          "text": [
-            "Login Successful"
+  var json_msg = null;
+
+  if(valid)***REMOVED***
+    json_msg = ***REMOVED***
+      "fulfillment_response": ***REMOVED***
+        "messages": [***REMOVED***
+          "text": ***REMOVED***
+            "text": [
+              "Login Successful"
+            ]
+          }
+        }],
+        "merge_behavior": "REPLACE"
+      },
+      /*"page_info":***REMOVED***
+        "form_info" : ***REMOVED***
+          "parameter_info": [
+            ***REMOVED***
+              "display_name": "valid",
+              "state": 3,
+              "value": true,
+            }
           ]
         }
-      }],
-      "merge_behavior": "REPLACE"
-    },
-    /*"page_info":***REMOVED***
-      "form_info" : ***REMOVED***
-        "parameter_info": [
-          ***REMOVED***
-            "display_name": "valid",
-            "state": 3,
-            "value": true,
-          }
-        ]
+      },*/
+      "session_info": ***REMOVED***
+        "parameters": ***REMOVED***
+          "loggedIn" : true,
+          "valid" : true
+        }
       }
-    },*/
-    "session_info": ***REMOVED***
-      "parameters": ***REMOVED***
-        "loggedIn" : true,
-        "valid" : true
+    }
+  }
+  else***REMOVED***
+    json_msg = ***REMOVED***
+      "fulfillment_response": ***REMOVED***
+        "messages": [***REMOVED***
+          "text": ***REMOVED***
+            "text": [
+              "Login Failed"
+            ]
+          }
+        }],
+        "merge_behavior": "REPLACE"
+      }, 
+      "session_info": ***REMOVED***
+        "parameters": ***REMOVED***
+          "loggedIn" : false,
+          "valid" : false
+        }
       }
     }
   }
